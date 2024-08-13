0c279741ab1805e25e29e4ad272d86555a87653a
Akshata-83500-KD2 <akshatawattamwar2@gmail.com>
2024-08-13 09:28:05 +0530

    forget password functionality with email by generating otp

--- BackEnd/spring_boot_backend_template/src/main/java/com/sunbeam/service/UserService.java
+++ BackEnd/spring_boot_backend_template/src/main/java/com/sunbeam/service/UserService.java
@@ -1,6 +1,8 @@
 package com.sunbeam.service;
 
+import com.sunbeam.dto.ForgotPasswordDTO;
 import com.sunbeam.dto.LoginDTO;
+import com.sunbeam.dto.ResetPasswordDTO;
 import com.sunbeam.dto.UserRegistrationDTO;
 import com.sunbeam.entities.User;
 
@@ -13,4 +15,7 @@ public interface UserService {
     void deleteUser(Long id);
     User registerUser(UserRegistrationDTO userRegistrationDTO);
     User login(LoginDTO loginDTO);
+    void generateAndSendOtp(ForgotPasswordDTO forgotPasswordDTO);
+    boolean resetPassword(ResetPasswordDTO resetPasswordDTO);
+    
 }
